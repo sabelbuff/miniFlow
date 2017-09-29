70f2904ff61760711c25a38ce0137dcb783def68
sabelbuff <singh.somanshu@gmail.com>
2017-09-28 21:55:58 -0400

    first commit

---
+++ miniflow.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Node(object):
+    def __init__(self, inbound_nodes = []):
+        self.inbound_nodes = inbound_nodes
+
+    outbound_nodes = []
+
+    value = None
+
+    for n in outbound_nodes:
+
+
+
