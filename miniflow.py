76d2e6f46bde89a2ccfb0f901b91bec3f3e26719
sabelbuff <singh.somanshu@gmail.com>
2017-09-28 23:55:37 -0400

    added classes for linear transform, sigmoid function and mean squared error function

--- miniflow.py
+++ miniflow.py
@@ -44,8 +44,41 @@ class Mul(Node):
         for n in self.inbound_nodes:
             temp_prod += n.value
         self.value = temp_prod
-    
 
+
+class Linear(Node):
+    def __int__(self, X, W, b):
+        Node.__init__(self, [X, W, b])
+
+    def forward(self):
+
+        X = self.inbound_nodes[0].value
+        W = self.inbound_nodes[1].value
+        b = self.inbound_nodes[2].value
+
+        self.value = np.dot(X, W) + b
+
+
+class Sigmoid(Node):
+    def __int__(self, x):
+        Node.__init__(self, [x])
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def forward(self):
+        input_value = self.inbound_nodes[0].value
+        self.value = self._sigmoid(input_value)
+
+
+class MSE(Node):
+    def __int__(self, y, a):
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        output_value = self.inbound_nodes[0].value.reshape(-1, 1)
+        target = self.inbound_nodes[1].value.reshape(-1, 1)
+        self.value = np.mean(np.square(target - output_value))
 
 
 
